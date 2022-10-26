91d371d51b35ffcf246d29c1f0e257185a8a405e
lanomade <ganne.hannah@gmail.com>
2022-10-27 00:02:20 +0200

    add url shortening business logic

--- src/components/InputLink.tsx
+++ src/components/InputLink.tsx
@@ -1,11 +1,54 @@
-import { InputContainer, Input } from '../styles/InputLink'
+import { useState } from 'react'
+import { Wrapper, InputContainer, Input } from '../styles/InputLink'
 import Button from './Button'
+import LinkContainer from './LinkContainer'
 
 export default function InputLink() {
+    const [link, setLink] = useState('')
+    const [error, setError] = useState(false)
+    const [isLoading, setIsLoading] = useState(true)
+    const [shortLink, setShortLink] = useState('')
+
+    console.log(link)
+
+    const handleSubmit = () => {
+        if (link === '') {
+            setError(true)
+            setIsLoading(true)
+        } else {
+            fetch(`https://api.shrtco.de/v2/shorten?url=${link}`)
+            .then(res => res.json())
+            .then(data => {
+                setError(false)
+                setShortLink(data.result.short_link)
+                setIsLoading(false)
+            })
+        }
+    }
+
     return (
-        <InputContainer>
-            <Input placeholder="Shorten a link here..." />
-            <Button label="Shorten It!" shape="" />
-        </InputContainer>
+        <Wrapper>
+            <InputContainer hasError={error}>
+                <div>
+                    <Input
+                        placeholder="Shorten a link here..."
+                        value={link}
+                        onChange={(e) => setLink(e.target.value)}
+                        hasError={error}
+                        onFocus={() => {
+                            setLink('')
+                            setIsLoading(true)
+                        }}
+                    />
+                    {error && <small>Please add a link</small>}
+                </div>
+                <Button
+                    label="Shorten It!"
+                    shape=""
+                    onClick={handleSubmit}
+                />
+            </InputContainer>
+            {(!error && !isLoading) && <LinkContainer link={link} shortLink={shortLink} /> }
+        </Wrapper>
     )
 }
