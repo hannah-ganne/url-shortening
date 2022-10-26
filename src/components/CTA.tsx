4646bdd541a016fbcf3e7e88c53c2f2334f2a9ad
lanomade <ganne.hannah@gmail.com>
2022-10-26 15:43:39 +0200

    create CTA component

---
+++ src/components/CTA.tsx
@@ -0,0 +1,13 @@
+import { CTAContainer } from '../styles/CTA'
+import Button from './Button'
+
+export default function CTA() {
+    return (
+        <CTAContainer>
+            <h2>
+                Boost your links today
+            </h2>
+            <Button label="Get Started" shape="round" />
+        </CTAContainer>
+    )
+}
