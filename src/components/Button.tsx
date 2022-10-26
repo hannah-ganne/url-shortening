0d2376056d97dc098f16fac7c4e5980123270560
lanomade <ganne.hannah@gmail.com>
2022-10-27 00:01:07 +0200

    add more props

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -3,12 +3,18 @@ import { ButtonContainer } from "../styles/Button"
 export interface Props {
     label: string
     shape: string
+    onClick?: any
+    isCopied?: boolean | undefined
 }
 
 export default function Button(props: Props) {
     return (
-        <ButtonContainer shape={props.shape}>
-            {props.label}
+        <ButtonContainer
+            onClick={props.onClick}
+            shape={props.shape}
+            isCopied={props.isCopied}
+        >
+            {props.isCopied? 'Copied!' : props.label}
         </ButtonContainer>
     )
 }
