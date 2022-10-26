319ba654141fbfa4f1f7a193a74449c601d75fb4
lanomade <ganne.hannah@gmail.com>
2022-10-27 00:01:40 +0200

    add a wrapper element

--- src/components/IconCard.tsx
+++ src/components/IconCard.tsx
@@ -1,4 +1,4 @@
-import { CardContainer, IconContainer } from '../styles/IconCard'
+import { Wrapper, CardContainer, IconContainer } from '../styles/IconCard'
 
 export interface Props {
     title: string
@@ -8,12 +8,14 @@ export interface Props {
 
 export default function IconCard(props: Props) {
     return (
-        <CardContainer>
-            <IconContainer>
-                <img src={props.icon} alt={`icon for ${props.title}`} />
-            </IconContainer>
-            <h3>{props.title}</h3>
-            <p>{props.description}</p>
-        </CardContainer>
+        <Wrapper>
+            <CardContainer>
+                <IconContainer>
+                    <img src={props.icon} alt={`icon for ${props.title}`} />
+                </IconContainer>
+                <h3>{props.title}</h3>
+                <p>{props.description}</p>
+            </CardContainer>
+        </Wrapper>
     )
 }
