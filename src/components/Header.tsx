563038678d3408beadbbb92a82f06e0f84dfc582
lanomade <ganne.hannah@gmail.com>
2022-10-26 16:50:34 +0200

    add onClick action for dropdown

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,14 +1,19 @@
 import logo from '../assets/logo.svg'
 import hamburger from '../assets/hamburger.svg'
 import { HeaderContainer } from '../styles/Header'
+import Dropdown from './Dropdown'
+import { useState } from 'react'
 
 export default function Header() {
+    const [open, setOpen] = useState(false)
+
     return (
         <HeaderContainer>
             <img src={logo} alt="logo of Shortly"></img>
-            <button>
+            <button onClick={() => setOpen(prev => !prev)}>
                 <img src={hamburger}></img>
             </button>
+            <Dropdown isOpen={open} />
         </HeaderContainer>
     )
 }
