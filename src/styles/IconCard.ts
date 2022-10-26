4220722b5c51835f941da5d2740370a7870a3a9d
lanomade <ganne.hannah@gmail.com>
2022-10-26 15:45:11 +0200

    create styled components for IconCard

---
+++ src/styles/IconCard.ts
@@ -0,0 +1,22 @@
+import styled from "styled-components";
+
+export const CardContainer = styled.article`
+    position: relative;
+    background-color: #fff;
+    border-radius: 0.4rem;
+    padding: 3.5rem 1.2rem 1.5rem 1.2rem;
+    margin-top: 5rem;
+`
+
+export const IconContainer = styled.div`
+    position: absolute;
+    top: -2.5rem;
+    left: 37%;
+    background-color: var(--dark-violet);
+    width: 5rem;
+    height: 5rem;
+    border-radius: 50%;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+`
