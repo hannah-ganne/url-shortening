cb59249ca39aa0d02ca0683c84ec6499baaa10c3
lanomade <ganne.hannah@gmail.com>
2022-10-27 00:01:31 +0200

    add a desktop nav bar

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -3,6 +3,9 @@ import hamburger from '../assets/hamburger.svg'
 import { HeaderContainer } from '../styles/Header'
 import Dropdown from './Dropdown'
 import { useState } from 'react'
+import { Nav, Ul } from '../styles/GlobalStyle'
+import ListItem from './ListItem'
+import Button from './Button'
 
 export default function Header() {
     const [open, setOpen] = useState(false)
@@ -10,10 +13,21 @@ export default function Header() {
     return (
         <HeaderContainer>
             <img src={logo} alt="logo of Shortly"></img>
-            <button onClick={() => setOpen(prev => !prev)}>
+            <button className="hamburger" onClick={() => setOpen(prev => !prev)}>
                 <img src={hamburger}></img>
             </button>
             <Dropdown isOpen={open} />
+            <Nav className="desktop-nav">
+                <Ul direction="row">
+                    <ListItem link="#" label="Features" />
+                    <ListItem link="#" label="Pricing" />
+                    <ListItem link="#" label="Resources" />
+                </Ul>
+                <Ul direction="row">
+                    <ListItem link="#" label="Login" />
+                    <ListItem label={<Button label="Sign Up" shape="round" />} />
+                </Ul>
+            </Nav>
         </HeaderContainer>
     )
 }
