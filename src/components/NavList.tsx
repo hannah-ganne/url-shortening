090db4c6b867b78189bb26e55e5d181a5f1a4260
lanomade <ganne.hannah@gmail.com>
2022-10-26 16:50:58 +0200

    make title and link props optional

--- src/components/NavList.tsx
+++ src/components/NavList.tsx
@@ -2,14 +2,14 @@ import { Ul } from '../styles/GlobalStyle'
 import ListItem from './ListItem'
 
 export interface Props {
-    title: string
-    data: { link: string; label: string }[]
+    title?: string
+    data: { link?: string; label: string | JSX.Element }[]
 }
 
 export default function NavList(props: Props) {
     return (
-        <Ul direction="column">
-        <h3>{props.title}</h3>
+    <Ul direction="column">
+        {props.title && <h3>{props.title}</h3>}
         {props.data.map(item => <ListItem link={item.link} label={item.label} />)}
     </Ul>
     )
