988b3ca58a96e696d6734fc78a98e1074e8c5b58
lanomade <ganne.hannah@gmail.com>
2022-10-26 16:51:08 +0200

    create styled components for dropdown

---
+++ src/styles/Dropdown.ts
@@ -0,0 +1,44 @@
+import styled, { keyframes } from "styled-components";
+
+// const growDown = keyframes`
+//     0% {
+//         transform: scaleY(0)
+//     }
+
+//     100% {
+//         transform: scaleY(1)
+//     }
+// `
+
+export const DropdownContainer = styled.nav<{ isOpen: boolean }>`
+    background-color: var(--dark-violet);
+    position: absolute;
+    top: 5rem;
+    left: 5%;
+    width: 90%;
+    border-radius: 0.5rem;
+    padding: 1.2rem 1.3rem 0 1.3rem;
+    transform-origin: top;
+    visibility: ${({ isOpen }) => isOpen ? `visible` : `hidden`};
+
+    & .divider {
+        border-top: 1px solid var(--grayish-violet);
+        opacity: 35%;
+        margin-bottom: 1.5rem;
+    }
+
+    & li {
+        margin-bottom: 1.5rem;
+    }
+
+    & a {
+        color: #fff;
+        font-size: 1rem;
+    }
+    
+    & button {
+        margin: 0;
+        padding: 0.5rem 0;
+        width: 100%;
+    }
+`
