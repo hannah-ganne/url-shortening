dbc5e1e9b0d53a2864ecb88184b6035088db331c
lanomade <ganne.hannah@gmail.com>
2022-10-27 00:03:17 +0200

    add conditional styling

--- src/styles/Button.ts
+++ src/styles/Button.ts
@@ -1,14 +1,15 @@
 import styled from "styled-components";
 
-export const ButtonContainer = styled.button<{ shape: string }>`
+export const ButtonContainer = styled.button<{ shape: string, isCopied?: boolean }>`
     font-size: 1.1rem;
     font-family: 'Poppins', sans-serif;
     font-weight: 700;
     letter-spacing: 0.05rem;
     color: #fff;
-    background-color: var(--cyan);
+    background-color: ${({ isCopied }) => isCopied ? 'var(--very-dark-violet)' : 'var(--cyan)'};
     border: none;
     padding: 0.7rem 2rem;
     margin-top: 1.5rem;
-    border-radius: ${({shape}) => shape === 'round' ? '5rem' : '0.4rem'}
+    border-radius: ${({ shape }) => shape === 'round' ? '5rem' : '0.4rem'};
+    cursor: pointer;
 `
