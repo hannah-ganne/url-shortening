43e71806118515765f31e3ebdf7fe1745ff3147c
lanomade <ganne.hannah@gmail.com>
2022-10-26 15:42:59 +0200

    render components

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,26 @@
 import React from 'react';
+import GlobalStyle from './styles/GlobalStyle'
+import Header from './components/Header'
+import { Main } from './styles/GlobalStyle'
+import Hero from './components/Hero'
+import InputLink from './components/InputLink';
+import Advantages from './components/Advantages';
+import CTA from './components/CTA'
+import Footer from './components/Footer'
 
 function App() {
   return (
-    <h1>Hello World!</h1>
+    <>
+      <GlobalStyle />
+      <Header />
+      <Main>
+        <Hero />
+        <InputLink />
+        <Advantages />
+        <CTA />
+      </Main>
+      <Footer />
+    </>
   )
 }
 
